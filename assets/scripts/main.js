fbc54c4f618588318dceb2695ed08023dc64ac7d
Maja Persson <persson.maja@gmail.com>
2017-12-18 15:14:39 +0100

    Event listener for delete button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('Hello World!');
+
+const danger = document.querySelector('.btn-danger');
+
+if (danger) {
+  danger.addEventListener('click', () => {
+    const reply = confirm('Are you sure?');
+    if(reply) {
+      window.location = '/delete.php';
+    }
+  })
+}
