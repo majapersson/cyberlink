ed827910ff4d687b486ad8bb11e6e98760e92f4c
Maja Persson <persson.maja@gmail.com>
2017-12-19 11:44:58 +0100

    Vote functionality

--- app/auth/vote.php
+++ app/auth/vote.php
@@ -3,12 +3,17 @@
 require __DIR__.'/../autoload.php';
 
 $post_id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
-$direction = filter_var($_GET['direction'], FILTER_SANITIZE_STRING);
+$direction = filter_var($_GET['dir'], FILTER_SANITIZE_NUMBER_INT);
+if (isset($_SESSION['user'])){
+    $user_vote = checkVote($pdo, $post_id, $_SESSION['user']['id']);
 
-if ($direction === 'up') {
-    updateScore($pdo, $post_id, 1);
-} else if ($direction === 'down') {
-    updateScore($pdo, $post_id, -1);
+    if (!$user_vote) {
+        setVote($pdo, $post_id, $_SESSION['user']['id'], $direction);
+    } else {
+        updateVote($pdo, $post_id, $_SESSION['user']['id'], $direction);
+    }
+
+    updateScore($pdo, $post_id, $direction);
 }
 
 redirect('/');
--- app/database/post_queries.php
+++ app/database/post_queries.php
@@ -101,7 +101,11 @@ function deletePost($pdo, $post_id) {
 }
 
 /**
+* Updates score for specific post
 *
+* @param PDO $pdo
+* @param int $post_id
+* @param int $direction
 */
 
 function updateScore($pdo, $post_id, $direction) {
@@ -115,5 +119,72 @@ function updateScore($pdo, $post_id, $direction) {
 
     $query-> bindParam(':score', $post['score'], PDO::PARAM_INT);
     $query-> bindParam(':id', $post_id, PDO::PARAM_INT);
+    $query-> execute();
+}
+
+/**
+ * Checks if user has already voted on specific post
+ *
+ * @param PDO $pdo
+ * @param int $post_id
+ * @param int $user_id
+ *
+ * @return array $user_vote
+ */
+
+function checkVote($pdo, $post_id, $user_id) {
+    $query = $pdo-> prepare('SELECT * from user_votes WHERE post_id=:post_id AND user_id=:user_id;');
+    if (!$query) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $query-> bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $query-> bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $query-> execute();
+
+    $user_vote = $query->fetch(PDO::FETCH_ASSOC);
+
+    return $user_vote;
+}
+
+/**
+ * Inserts new vote into user_votes table
+ *
+ * @param PDO $pdo
+ * @param int $post_id
+ * @param int $user_id
+ * @param int $direction
+ */
+
+function setVote($pdo, $post_id, $user_id, $direction) {
+    $query = $pdo-> prepare('INSERT INTO user_votes (post_id, user_id, direction) VALUES (:post_id, :user_id, :dir);');
+    if (!$query) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $query-> bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $query-> bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $query-> bindParam(':dir', $direction, PDO::PARAM_INT);
+    $query-> execute();
+}
+
+/**
+ * Updates existing vote in user_votes table
+ *
+ * @param PDO $pdo
+ * @param int $post_id
+ * @param int $user_id
+ * @param int $direction
+ */
+
+function updateVote($pdo, $post_id, $user_id, $direction) {
+    $query = $pdo-> prepare('UPDATE user_votes SET direction=:dir WHERE post_id=:post_id AND user_id=:user_id;');
+    if (!$query) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $query-> bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $query-> bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $query-> bindParam(':dir', $direction, PDO::PARAM_INT);
     $query-> execute();
 }
