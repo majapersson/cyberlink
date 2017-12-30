08aa0dfa6337549b0672760a19741fdb8859cd98
Maja Persson <persson.maja@gmail.com>
2017-12-30 01:11:45 +0100

    Added vote.js to index

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,6 @@
     </div><!-- /container -->
 
+<script type="text/javascript" src="../assets/scripts/vote.js"></script>
 <script type="text/javascript" src="../assets/scripts/main.js"></script>
 </body>
 </html>
